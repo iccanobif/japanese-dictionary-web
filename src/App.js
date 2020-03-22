bccc859c119a291f3dbf30228a9b8db1c17718d4
iccanobif <iccanobif@protonmail.com>
2020-03-22 11:12:13 +0100

    stub

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+//<img src={logo} className="App-logo" alt="logo" />
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component
+{
+  render()
+  {
+    const results = ['result 1', 'result 2']
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input type="text"></input>
+          <SearchResults results={results}></SearchResults>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+class SearchResults extends Component
+{
+  render()
+  {
+    return this.props.results
+      .map(r => (<div>{r}</div>))
+  }
+}
