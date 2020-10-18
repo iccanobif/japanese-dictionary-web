854d2f3790399171a4e07f803bd136144a94fdb1
iccanobif <iccanobif@protonmail.com>
2020-10-18 11:45:06 +0200

    integrated dictionary popup

---
+++ src/IntegratedDictionaryOpener.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+
+export default function IntegratedDictionaryOpener()
+{
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [validated, setValidated] = useState(false)
+  const [url, setUrl] = useState("")
+
+  const handleSubmit = (event) =>
+  {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const form = event.currentTarget;
+    if (!form.checkValidity())
+    {
+      setValidated(true);
+    }
+    else
+    {
+      const newUrl = "http://japdictapi.herokuapp.com/integrated-dictionary/" +
+        (url.toLowerCase().startsWith("http://") || url.toLowerCase().startsWith("https://") ? url : "http://" + url)
+      window.open(newUrl)
+      setUrl("")
+      setValidated(false)
+      setIsModalVisible(false)
+    }
+  };
+
+  return (
+    <>
+      <Button size="sm" variant="outline-secondary" onClick={() => setIsModalVisible(true)}>統合辞書</Button>
+      <Modal show={isModalVisible} onHide={() => setIsModalVisible(false)} >
+        <Modal.Header closeButton>
+          <Modal.Title>統合辞書</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form onSubmit={handleSubmit} noValidate validated={validated}>
+            <Form.Group controlId="url">
+              <Form.Label>Url:</Form.Label>
+              <Form.Control type="text" placeholder="Urlを入力して下さい" required value={url} onChange={(e) => setUrl(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit" >Go</Button>
+          </Form>
+        </Modal.Body>
+      </Modal></>
+  )
+}
