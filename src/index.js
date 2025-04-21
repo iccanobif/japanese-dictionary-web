90d54b0fd6c5446bc309051909002ef422e84bc5
iccanobif <iccanobif@protonmail.com>
2025-04-21 11:04:47 +0200

    upgrade to react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -16,15 +16,11 @@ const middlewares = [
 
 export const store = createStore(reducer, applyMiddleware(...middlewares));
 
-ReactDOM.render(
-  // <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  // </React.StrictMode>
-  ,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Provider store={store}>
+  <App />
+</Provider>)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
